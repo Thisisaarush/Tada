64b4298c043830c143cc80c90f98afa8e723bef3
Aarush Tanwar <Aarush.tanwar-ext@ab-inbev.com>
2023-06-24 15:20:26 +0530

    removed unused code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Header from './Header.svelte';
+	import Header from './header.svelte';
 	import '../app.css'
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,6 @@
 	<meta name="description" content="Todo app" />
 </svelte:head>
 
-<section class='h-[5000px]'>
+<section>
 
 </section>
