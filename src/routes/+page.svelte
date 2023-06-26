f13492e59394aa72eafd4e214fa19b97fe1f14a6
Aarush Tanwar <Aarush.tanwar-ext@ab-inbev.com>
2023-06-26 15:23:25 +0530

    added task component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,18 @@
 <script>
 	import TaskTitle from './TaskTitle.svelte'
 	import AddTask from './AddTask.svelte'
+	import Task from './Task.svelte'
+
+  import doingIcon from '$lib/assets/doing.svg'
+  import tickIcon from '$lib/assets/tick.svg'
+  import greenTickIcon from '$lib/assets/greenTick.svg'
+
+	const taskProps = {
+		icon: tickIcon,
+		title: 'connect with team on zoom for discussion',
+		tags: ['medium', 'off track'],
+		time: 'Today, 5:00 pm'
+	}
 </script>
 
 <svelte:head>
@@ -8,16 +20,17 @@
 	<meta name="description" content="Todo app" />
 </svelte:head>
 
-<section class='mt-44 mb-10 flex flex-col md:flex-row justify-center items-center gap-8 p-2'>
-	<div class='max-w-xs w-5/6 flex flex-col gap-4'>
+<section class='mt-44 mb-10 flex flex-col md:flex-row justify-center items-center md:items-start gap-8 p-2'>
+	<div class='max-w-xs w-5/6 flex flex-col gap-6'>
 		<TaskTitle title='todo' bgColor='bg-blue-200'/>
+		<Task {...taskProps} />
 		<AddTask />
 	</div>
-	<div class='max-w-xs w-5/6 flex flex-col gap-4'>
+	<div class='max-w-xs w-5/6 flex flex-col gap-6'>
 		<TaskTitle title='doing' bgColor='bg-yellow-200'/>
 		<AddTask />
 	</div>
-	<div class='max-w-xs w-5/6 flex flex-col gap-4'>
+	<div class='max-w-xs w-5/6 flex flex-col gap-6'>
 		<TaskTitle title='done' bgColor='bg-green-200'/>
 		<AddTask />
 	</div>
---
+++ src/routes/Task.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+	import clockIcon from '$lib/assets/clock.svg'
+
+  // props
+  export let icon: string;
+  export let title: string;
+  export let tags: string[];
+  export let time: string;
+</script>
+
+<div class='flex flex-col justify-between items-center border rounded-lg p-4 capitalize gap-12'>
+  <div class='flex items-start justify-center gap-4'>
+    <img src={icon} alt="tick" class='mt-2'>
+    <div class='flex flex-col gap-2'>
+      <p class='font-semibold line-clamp-2'>{title}</p>
+      <span class='flex gap-2'>
+        { #each tags as tag }
+        <p class={`py-1 px-2 bg-gray-100 rounded-lg text-sm`}>{tag}</p>
+        {/each}
+      </span>
+    </div>
+  </div>
+
+  <div class='flex justify-between text-sm items-center w-full'>
+    <p class='text-gray-500'>more details</p>
+    <span class='flex gap-2'>
+      <p class='uppercase'>{time}</p>
+      <img src={clockIcon} alt="">
+    </span>
+  </div>
+</div>
