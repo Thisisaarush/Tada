8cc9d8cef79724ce6c2a754669c6ff1d10763695
Aarush Tanwar <tanwaraarush007@gmail.com>
2023-06-27 01:15:35 +0530

    refactored responsiveness

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,18 +1,16 @@
 <script>
-  import profileIcon from '$lib/assets/profile.svg'
   import moment from 'moment'
 
   const day = moment().format('dddd') + " " + moment().format('MMMM DD')
 </script>
 
 <header class='fixed top-8 flex justify-center items-center flex-col gap-2 w-full'>
-  <ul class='flex gap-2 items-center justify-center border rounded-2xl py-2 backdrop-blur-md w-5/6 max-w-xl'>
+  <ul class='flex gap-2 items-center justify-center border rounded-2xl py-2 sm:px-4 backdrop-blur-md w-[95%] sm:w-fit max-w-xl'>
     <a href="/" class="uppercase font-bold text-2xl py-2 px-4 rounded-md">Tada.</a>
     <li class="text-xl">|<li/>
     <a href="/pomo" class="capitalize text-lg px-4 py-2 rounded-md hover:bg-gray-100">pomo</a>
-    <a href='/stopwatch' class="hidden sm:block capitalize text-lg px-4 py-2 rounded-md hover:bg-gray-100">stopwatch</a>
+    <a href='/stopwatch' class="capitalize text-lg px-4 py-2 rounded-md hover:bg-gray-100">stopwatch</a>
     <a href="/calendar" class="hidden sm:block capitalize text-lg px-4 py-2 rounded-md hover:bg-gray-100">Calendar</a>
-    <img src={profileIcon} alt="user profile" class="rounded-md p-1">
   </ul>
   <p class="uppercase tracking-[0.475rem] text-gray-500 text-sm">{day}</p>
 </header>
