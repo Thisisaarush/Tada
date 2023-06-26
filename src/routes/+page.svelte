072922bed513a9af3496459631d7fa6395fa9f69
Aarush Tanwar <Aarush.tanwar-ext@ab-inbev.com>
2023-06-27 00:45:35 +0530

    implemented drag & drop functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,51 @@
-<script>
+<script lang='ts'>
 	import TaskTitle from './TaskTitle.svelte'
 	import AddTask from './AddTask.svelte'
 	import Task from './Task.svelte'
 
-  import doingIcon from '$lib/assets/doing.svg'
   import tickIcon from '$lib/assets/tick.svg'
+  import doingIcon from '$lib/assets/doing.svg'
   import greenTickIcon from '$lib/assets/greenTick.svg'
 
-	const taskProps = {
-		icon: tickIcon,
-		title: 'connect with team on zoom for discussion',
-		tags: ['medium', 'off track'],
-		time: 'Today, 5:00 pm'
+	let tasksData = [
+		{
+			title: 'todo', 
+			bgColor: 'bg-blue-200',
+			items: [
+				{
+					id: "1",
+					icon: tickIcon,
+					title: 'connect with team on zoom for discussion',
+					tags: ['medium', 'off track'],
+					time: 'Today, 5:00 pm',
+				},
+				{
+					id: "2",
+					icon: tickIcon,
+					title: '2 connect with team on zoom for discussion',
+					tags: ['easy', 'off track'],
+					time: 'Today, 6:00 pm',
+				}
+			]
+		},
+		{
+			title: 'doing', 
+			bgColor: 'bg-yellow-200',
+			items: []
+		},
+		{
+			title: 'done', 
+			bgColor: 'bg-red-300',
+			items: []
+		},
+	]
+
+	const handleDrop = (e: any, taskIndex: number) => {
+		e.preventDefault();
+		const data = JSON.parse(e.dataTransfer.getData("text/plain"))
+		const [item] = tasksData[data.taskIndex].items.splice(data.itemIndex, 1);
+		tasksData[taskIndex].items.push(item);
+		tasksData = tasksData;
 	}
 </script>
 
@@ -21,17 +55,15 @@
 </svelte:head>
 
 <section class='mt-44 mb-10 flex flex-col md:flex-row justify-center items-center md:items-start gap-8 p-2'>
-	<div class='max-w-xs w-5/6 flex flex-col gap-6'>
-		<TaskTitle title='todo' bgColor='bg-blue-200'/>
-		<Task {...taskProps} />
-		<AddTask />
-	</div>
-	<div class='max-w-xs w-5/6 flex flex-col gap-6'>
-		<TaskTitle title='doing' bgColor='bg-yellow-200'/>
-		<AddTask />
-	</div>
-	<div class='max-w-xs w-5/6 flex flex-col gap-6'>
-		<TaskTitle title='done' bgColor='bg-green-200'/>
-		<AddTask />
-	</div>
+	{#each tasksData as {title, bgColor, items}, taskIndex }
+		<div class='max-w-xs w-5/6 flex flex-col gap-6'>
+			<TaskTitle title={title} bgColor={bgColor}/>
+			<ul class='min-h-[200px] flex flex-col gap-4' on:dragover={(e) => e.preventDefault()} on:drop={(e) => handleDrop(e, taskIndex)}>
+				{#each items as item, itemIndex }
+						<Task {...item} itemIndex={itemIndex} taskIndex={taskIndex}/>
+				{/each}
+			</ul>
+			<AddTask />
+		</div>
+	{/each}
 </section>
--- src/routes/Task.svelte
+++ src/routes/Task.svelte
@@ -2,27 +2,36 @@
 	import clockIcon from '$lib/assets/clock.svg'
 
   // props
+  export let id: string;
   export let icon: string;
   export let title: string;
   export let tags: string[];
   export let time: string;
+  export let taskIndex: number
+  export let itemIndex: number
+
+  const handleDragStart = (e: any, taskIndex: number, itemIndex: number) => {
+    const data = {taskIndex, itemIndex};
+    e.dataTransfer.setData('text/plain', JSON.stringify(data))
+  }
 </script>
 
-<div class='flex flex-col justify-between items-center border rounded-lg p-4 capitalize gap-12'>
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div id={id} class='flex flex-col justify-between items-center border bg-white rounded-lg p-4 capitalize gap-12' draggable={true} on:dragstart={(e) => handleDragStart(e, taskIndex, itemIndex)} >
   <div class='flex items-start justify-center gap-4'>
     <img src={icon} alt="tick" class='mt-2'>
     <div class='flex flex-col gap-2'>
       <p class='font-semibold line-clamp-2'>{title}</p>
       <span class='flex gap-2'>
         { #each tags as tag }
-        <p class={`py-1 px-2 bg-gray-100 rounded-lg text-sm`}>{tag}</p>
+        <p class={`py-1 px-2 bg-black text-white rounded-lg text-sm`}>{tag}</p>
         {/each}
       </span>
     </div>
   </div>
 
   <div class='flex justify-between text-sm items-center w-full'>
-    <p class='text-gray-500'>more details</p>
+    <p class='text-gray-500 hover:underline cursor-pointer underline-offset-2'>more details</p>
     <span class='flex gap-2'>
       <p class='uppercase'>{time}</p>
       <img src={clockIcon} alt="">
